ec94ea979405bba05245a0276ec40eb5d2d54f58
Shakhawat <shshamim63@yahoo.comgit config --global user.name Shakhawatgit comitgit commitgit commitgit config --global user.email shshamim63@yahoo.com>
2019-01-22 12:39:15 +0600

    deleted x+y print

--- hello.py
+++ hello.py
@@ -1,10 +1,8 @@
 x=5
 y=15
-print(x+y)
-<<<<<<< HEAD
-print("Her age is the sum of x+y= " + str(x+y))
-=======
 Name_first= "Shakhawat"
 Name_second="Hossain"
 print(Name_first +" " +Name_second)
->>>>>>> branchA
+User="shshamim63"
+password="1234"
+print(User + " "+ password)
