ce504a1306cf624ae4d8298db26852669101a6e6
Shakhawat <shshamim63@yahoo.comgit config --global user.name Shakhawatgit comitgit commitgit commitgit config --global user.email shshamim63@yahoo.com>
2019-01-22 12:55:44 +0600

    merged anotherbranch

--- hello.py
+++ hello.py
@@ -1,10 +1,8 @@
-x=5
-y=15
 Name_first= "Shakhawat"
 Name_second="Hossain"
 print(Name_first +" " +Name_second)
-
 User="shshamim63"
 password="1234"
 print(User + " "+ password)
 print("new merge now")
+
